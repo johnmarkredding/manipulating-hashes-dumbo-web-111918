48641debe3c12c731ace553478935693f930b6d0
John Mark Redding <johnmarkredding@gmail.com>
2018-11-11 03:18:37 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,7 +19,7 @@ def first_challenge
     if person == "Freddy Mercury"
       data.each do |attribute, value|
         if attribute == :favorite_icecream_flavors
-          value.delete_if("strawberry")
+          value.delete("strawberry")
         end
       end
     end
